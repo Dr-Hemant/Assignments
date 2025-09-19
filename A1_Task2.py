6282edcafce684d9c41a85555995c5682c9ec7f6
Dr-Hemant <hemantkumarjangid122001@gmail.com>
2025-09-19 09:33:16 +0530

    Assignment 1 Completed

--- A1_Task2.py
+++ A1_Task2.py
@@ -1,1 +1,4 @@
-
+#Create a Personalized Greeting
+firstname = input("Enter your first name :")
+lastname  = input("Enter your last name:")
+print(f"Hello, {firstname} {lastname}! Welcome to the python program.")
